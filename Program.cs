69895a4233a5c7435c16196480311e0cae27502b
Dwiki Ikhwan <dwikiikhwan13@gmail.com>
2017-05-30 15:03:06 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp1.ExtentionMethod;
+using ConsoleApp1.Model;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PesertaBootcamp peserta = new PesertaBootcamp();
+            try
+            {
+                peserta.mainMenu();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Inputan salah");
+                peserta.toMainMenu();
+            }
+        }
+
+    }
+}
